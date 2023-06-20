59b7fe517c411812f4c3ca36fd1329d4dd09495d
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-06-19 19:49:23 -0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let con = [".com", ".org", ".net", ".cr"];
+var i = null
+var j = null
+var x = null
+var l = null
+for (i = 0; i <= 1; i++) {
+    var paso1 = pronoun[i];
+    for (j = 0; j <= 1; j++) {
+        var paso2 = adj[j];
+        for (x = 0; x <= 1; x++) {
+            var paso3 = noun[x];
+            for (l = 0; l <= 3; l++) {
+                var paso4 = con[l];
+                console.log(paso1 + paso2 + paso3 + paso4);
+                
+            }
+        }
+    }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
